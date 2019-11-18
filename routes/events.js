4cb9300de65c41e081564f079e287608400054d4
Mark Ford <mark@intelligentli.me>
2019-11-18 14:26:09 +0000

    remove meetup api to fix site

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var async = require('async');
 var router = express.Router();
-var meetup = require('meetup-api')({
-	key: process.env.MEETUP_API
 });
 
 router.get('/', indexRoute);
@@ -10,15 +8,12 @@ router.get('/', indexRoute);
 module.exports = router;
 
 function indexRoute(req, res, next) {
-	async.parallel({ group: getGroup, events: getEvents }, renderIndex.bind(null, res, next));
 }
 
 function getGroup(callback) {
-	meetup.getGroup({urlname: process.env.MEETUP_GROUP_NAME, fields: 'group_photo'}, callback);
 }
 
 function getEvents(callback) {
-	meetup.getEvents({'group_urlname': process.env.MEETUP_GROUP_NAME}, callback);
 }
 
 function renderIndex(res, next, err, results) {
@@ -26,5 +21,4 @@ function renderIndex(res, next, err, results) {
 		return next(err);
 	}
 
-	res.render('events', { group: results.group, events: results.events.results });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var async = require('async');
 var router = express.Router();
-var meetup = require('meetup-api')({
-	key: process.env.MEETUP_API
-});
 
-router.get('/', indexRoute);
+
+router.get('/', function(req, res){
+	res.render('index.html')
+});
 
 router.get('/team', function(req, res, next) {
 	res.render('team')
@@ -16,22 +16,3 @@ router.get('/theendofdigital', function(req, res, next) {
 
 module.exports = router;
 
-function indexRoute(req, res, next) {
-	async.parallel({ group: getGroup, events: getEvents }, renderIndex.bind(null, res, next));
-}
-
-function getGroup(callback) {
-	meetup.getGroup({urlname: process.env.MEETUP_GROUP_NAME, fields: 'group_photo'}, callback);
-}
-
-function getEvents(callback) {
-	meetup.getEvents({'group_urlname': process.env.MEETUP_GROUP_NAME}, callback);
-}
-
-function renderIndex(res, next, err, results) {
-	if (err) {
-		return next(err);
-	}
-
-	res.render('index', { group: results.group, events: results.events.results });
-}
